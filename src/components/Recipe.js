345d1d1ec5a7b87f2380a1116b2b57a99e0e5cd5
rocio-milla <rocio.milla@moxit.net>
2020-09-16 11:39:55 -0300

    Added images in recipes list

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,6 +5,12 @@ const Recipe = ({ recipe }) => {
         <div className="col-md-4 mb-3">
             <div className="card">
                 <h2 className="card-header">{recipe.strDrink}</h2>
+                <img className="card-img-top" src={recipe.strDrinkThumb} alt={`${recipe.strDrink} recipe`} />
+                <div className="card-body">
+                    <button type="button" className="btn btn-block btn-primary">
+                        Read recipe
+                    </button>
+                </div>
             </div>
         </div>
     );
